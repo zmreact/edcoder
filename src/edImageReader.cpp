3a0c8b287cfe13652862edb051fc6df0c959bd60
Denis <zmreact@gmail.com>
2017-04-21 21:20:05 +0300

    Новый класс edImageReader. Исправил баги (не выводилась информация об изображении). Переписал конструктор.

--- src/edImageReader.cpp
+++ src/edImageReader.cpp
@@ -1,23 +1,22 @@
 #include "edImageReader.h"
 
-edImageReader::edImageReader(QString val)
-{
-    path = val;
-}
-
-void edImageReader::read_pixel(int i, int j)
+edImageReader::edImageReader(QString path)
 {
     img.load(path);
     if (img.isNull()) {
         read_image_ok = false;
     } else {
-        QRgb *rowData = (QRgb *) img.scanLine(i);
-        QRgb pixelData = rowData[j];
-        red = qRed(pixelData);
-        green = qGreen(pixelData);
-        blue = qBlue(pixelData);
         read_image_ok = true;
     }
+}
+
+void edImageReader::read_pixel(int i, int j)
+{
+    QRgb *rowData = (QRgb *) img.scanLine(i);
+    QRgb pixelData = rowData[j];
+    red = qRed(pixelData);
+    green = qGreen(pixelData);
+    blue = qBlue(pixelData);
 }
 
 void edImageReader::image_info(QTextStream &out)
@@ -33,16 +32,12 @@ void edImageReader::image_info(QTextStream &out)
 
 void edImageReader::pixel_data(QTextStream &out)
 {
-    img.load(path);
-    if (img.isNull()) {
-        read_image_ok = false;
-    } else {
+    if (read_image_ok) {
         for (int i = 0; i < img.height(); i++) {
             for (int j = 0; j < img.width(); j++) {
                 read_pixel(i, j);
                 out << i*img.width()+j+1 << " " << "pixel data: " << "R" << red << "G" << green << "B" << blue << endl;
             }
         }
-        read_image_ok = true;
     }
 }
--- src/edImageReader.h
+++ src/edImageReader.h
@@ -8,16 +8,15 @@
 class edImageReader
 {
 private:
-    QString path;
     QImage img;
     QRgb pixelData;
-    bool read_image_ok;
     int red, green, blue;
+    bool read_image_ok = false;
 public:
     void pixel_data(QTextStream &);
     void image_info(QTextStream &);
     void read_pixel(int, int);
-    edImageReader(QString val);
+    edImageReader(QString path);
 };
 
 
